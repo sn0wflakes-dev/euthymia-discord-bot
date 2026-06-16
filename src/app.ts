18d2c92b7578309c27c7de09eed9aa70be4c75ac
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-06-16 23:35:19 +0000

    init: add basic interaction and command deployer

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,70 @@
 import { Hono } from 'hono'
+import {Events, Client, GatewayIntentBits, Collection, REST} from "discord.js";
+import {join} from "node:path"
+import {readdirSync} from "node:fs"
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+export const token = process.env.DISCORD_TOKEN;
+export const clientId = process.env.DISCORD_APPLICATION_ID;
+export const guildId = process.env.DISCORD_GUILD_ID;
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]})
+
+client.once(Events.ClientReady, (clientReady) => {
+    console.log(`Ready logged-in as ${clientReady.user.tag}`)
 })
 
+client.login(token)
+
+client.commands = new Collection();
+
+const dirPath = join(__dirname, 'commands')
+const commandDir = readdirSync(dirPath);
+
+(async () => {
+    for (const directory of commandDir) {
+        const commandPath = join(dirPath, directory);
+        const commandFile = readdirSync(commandPath).filter((file) => file.endsWith('.ts'));
+
+        for (const file of commandFile) {
+            const filePath = join(commandPath, file);
+            const command = (await import(filePath)).default;
+
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing required data or execute property`);
+            }
+        }
+    }
+})();
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`[ERR] No command matching ${interaction.commandName} was found`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command !',
+                flags: MessageFlags.Ephemeral
+            })
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command!',
+                flags: MessageFlags.Ephemeral,
+            })
+        }
+    }
+})
 export default app
---
+++ src/commands/utils/ping.ts
@@ -0,0 +1,8 @@
+import {SlashCommandBuilder} from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder().setName('ping').setDescription('Reply with pong !'),
+    async execute(interaction) {
+        await interaction.reply(`Hello ${interaction.user.username}, good morning !`);
+    },
+};
